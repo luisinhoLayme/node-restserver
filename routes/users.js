a78127765461af0007e0d09cb9aef0753ca11325
Luisinho Layme <luisinhotf@gmail.com>
2021-11-15 11:15:48 -0400

    RestServer Listo

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,16 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
 
-const {validarCampos} = require('../middlewares/validar-campos');
-const {validarJWT} = require('../middlewares/validar-jwt');
-const {esAdminRole, tieneRol} = require('../middlewares/validar-roles');
+/* const {validarCampos} = require('../middlewares/validar-campos'); */
+/* const {validarJWT} = require('../middlewares/validar-jwt'); */
+/* const {esAdminRole, tieneRol} = require('../middlewares/validar-roles'); */
+
+const {
+  validarCampos,
+  validarJWT,
+  esAdminRole,
+  tieneRol
+} = require('../middlewares')
 
 const {
   usersGet,
@@ -39,8 +46,8 @@ router.post('/', [
 
 router.delete('/:id',[
   validarJWT,
-  // esAdminRole,
-  tieneRol('NOSE_ROLE', 'VENTAS_ROLE'),
+  /* esAdminRole, */
+  tieneRol('ADMIN_ROLE', 'VENTAS_ROLE'),
   check('id', 'No es un ID valido').isMongoId(),
   check('id').custom( existeUsuarioPorId ),
   validarCampos
